5bf79b9e54b43453d208ec6c609cd6447996b860
Luciano Almeida <lucianopa@microsoft.com>
2024-09-30 18:47:27 -0300

    [Common] Vision OS support

--- sdk/communication/AzureCommunicationCommon/Package.swift
+++ sdk/communication/AzureCommunicationCommon/Package.swift
@@ -32,7 +32,7 @@ import PackageDescription
 let package = Package(
     name: "AzureCommunicationCommon",
     platforms: [
-        .macOS(.v10_15), .iOS(.v12)
+        .macOS(.v10_15), .iOS(.v12), .visionOS(.v1)
     ],
     products: [
         .library(name: "AzureCommunicationCommon", targets: ["AzureCommunicationCommon"])
